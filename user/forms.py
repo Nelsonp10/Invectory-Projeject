d172f6e4b7e742e46a4fde56184175f8b483100a
Nelsonp10 <nelsonapps7@gmail.com>
2026-02-19 12:04:02 +0200

    criei pagina de actualizacao de perfil

--- user/forms.py
+++ user/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from  .models import profile
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm  
 
@@ -7,3 +8,15 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email']
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model= User
+        fields = ['username', 'email']
+
+
+class profileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = profile
+        fields = ['address', 'phone','image']
