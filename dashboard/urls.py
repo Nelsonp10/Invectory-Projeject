d67c03da81bd88366ea03ae90e1559bbfb31c9ce
Nelsonp10 <nelsonapps7@gmail.com>
2026-03-02 14:47:20 +0200

    Finalizei CRUD_Functions

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,6 +5,8 @@ from .import views
 urlpatterns = [
     path('dashboard/', views.dashboard_view, name='dashboard-index'),  
     path('staff/', views.staff_view, name='dashboard-staff'),
-    path('products/', views.products_view, name='dashboard-products'),
     path('orders/', views.orders_view, name='dashboard-orders'),
+    path('products/', views.products_view, name='dashboard-products'),
+    path('product/delete/<int:pk>/', views.product_delete, name='dashboard-product-delete'),
+    path('product/update/<int:pk>/', views.product_update, name='dashboard-product-update'),
 ]   
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from.models import product
 from django.http import HttpResponse
+from .models import product
+from .forms import ProductForm
 
 @login_required
 def dashboard_view(request):
@@ -13,7 +15,45 @@ def staff_view(request):
 
 @login_required
 def products_view(request):
-    return render(request, 'dashboard/product.html')
+    items =product.objects.all()
+   #items = product.objects.raw('SELECT * FROM dashboard_product')
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+         form.save()
+        return redirect('dashboard-products')
+    else:
+        form = ProductForm()
+    context = {
+        'items': items,
+        'form':form,
+    }
+    return render(request, 'dashboard/product.html',context)
+
+def product_delete(request,pk):
+    item = product.objects.get(id=pk)
+    if request.method =='POST':
+        item.delete()
+        return redirect('dashboard-products')
+    
+    return render(request, 'dashboard/product_delete.html')
+
+def product_update(request,pk):
+    item = product.objects.get(id=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect ('dashboard-products')
+            
+    else:
+        form = ProductForm(instance = item)
+    context= {
+        'form': form,
+
+        }
+    return render(request,'dashboard/product_update.html',context)
+
 
 @login_required
 def orders_view(request):
@@ -21,4 +61,5 @@ def orders_view(request):
 
 def register(request):
     return render(request, 'dashboard/register.html')   
+
 
